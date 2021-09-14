576a16a7ebe911be9e6658c888174daeeb7eca28
ohjinho <ohjinho7@gmail.com>
2021-09-14 20:27:57 +0900

    JPA 영속성관리(영속성 컨텍스트)

--- JPA/김영한_강의/jpa_basic/src/main/java/hellojpa/JpaMain.java
+++ JPA/김영한_강의/jpa_basic/src/main/java/hellojpa/JpaMain.java
@@ -31,16 +31,62 @@ public class JpaMain {
 //        findMember.setName("helloJPA");
 //
 //        tx.commit();
+//
+//        List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                .setFirstResult(5)
+//                .setMaxResults(8)
+//                .getResultList();
+//
+//        for (Member member : result) {
+//            System.out.println("member.getName() = " + member.getName());
+//        }
 
-        List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                .setFirstResult(5)
-                .setMaxResults(8)
-                .getResultList();
+        //비영속
+//        Member member = new Member();
+//        member.setId(101L);
+//        member.setName("HelloJPAAA");
+//
+//        //영속
+//        System.out.println("=== BEFORE ===");
+//        em.persist(member);
+//        System.out.println("=== AFTER ===");
 
-        for (Member member : result) {
-            System.out.println("member.getName() = " + member.getName());
-        }
+//        Member findMember1 = em.find(Member.class, 101L);
+//        Member findMember2 = em.find(Member.class, 101L);
+//        // 이처럼 조회를 2번할 시에는, 1번은 1차캐시에서 데이터를 가지고 온다.
+//
+//        System.out.println(findMember1 == findMember2);
+//        //영속성 콘텍스트
+//
+//        Member member1 = new Member(150L, "A");
+//        Member member2 = new Member(151L, "B");
+//
+//        em.persist(member1);
+//        em.persist(member2);
+////        System.out.println("findMember.id = " + findMember.getId());
+////        System.out.println("findMember.getName() = " + findMember.getName());
 
+//        Member member = em.find(Member.class, 150L);
+//        member.setName("ABCD");
+//
+//        //  em.persist(member) 할 필요 없음
+//
+//        System.out.println("====================");
+//        Member member = new Member(200L, "member200");
+//        em.persist(member);
+//
+//        em.flush();
+//
+        Member member = em.find(Member.class, 150L);
+        member.setName("ABCD");
+
+        //em.detach(member);
+        //em.clear();
+        //em.close();
+        
+        System.out.println("====================");
+
+        tx.commit();
     } catch(Exception e) {
         tx.rollback();
         }
