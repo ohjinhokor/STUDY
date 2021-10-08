2250cc497610b446d01685506238abd69c1b6e9f
ohjinho <ohjinho7@gmail.com>
2021-10-08 19:18:25 +0900

    코틀린 1강

---
+++ Kotlin/Kotlin_basic/src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    // 1. 기본
+//    println("HelloKotlin");
+//    println("hi")
+
+    // 2. 파라미터 사용
+//    println("args[0] = ${args[0]}")
+//    println(args[1])
+    
+}
