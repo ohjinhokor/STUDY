c85b5bef6cfa348d07d18d3cbfe33810dff658cd
ohjinho <ohjinho7@gmail.com>
2021-10-09 21:04:44 +0900

    코틀린 1 - 2강

--- Kotlin/Kotlin_basic/src/main/kotlin/HelloKotlin.kt
+++ Kotlin/Kotlin_basic/src/main/kotlin/HelloKotlin.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main() {
     // 1. 기본
 //    println("HelloKotlin");
 //    println("hi")
@@ -6,5 +6,96 @@ fun main(args: Array<String>) {
     // 2. 파라미터 사용
 //    println("args[0] = ${args[0]}")
 //    println(args[1])
+
+    // 3. 자료형
+//    val username: String = "Kildong";
+//    // 값 변경 불가능  username = "dooly"
+//
+//    var my_name: String = "jinho"
+//    my_name = "ohjinho"
+//
+//    var count = 3 // 타입 생략가능
+//
+//    println("username : $username")
+//    println("myname : $my_name")
+//    println("count $count")
+//
+//    val user = "kildong"
+//    // 사용 불가능 var username
+//    val init: Int
+//    val str : String8
+//    val number = 10
+//
+//    val exp1 = 123
+//    val exp2 = 123L
+//    val exp3 = 0x0F
+//
+//    val exp4 : Byte = 127
+//    val exp5 = 1234
+//    val exp6 : Short = 127
+//    val exp7 = 1_000_000
+//
+//    println("IntMax : ${Int.MAX_VALUE}")
+//    println("IntMin : ${Int.MIN_VALUE}")
+
+//    var str1 = "Hello"
+//    var str2 = "World"
+//    var str3 = "Hello"
+//
+//    println("str1 === str2 :  ${str1 === str2}")
+//    println("str1 === str3 :  ${str1 === str3}") // === 는 참조하는 곳까지 비교할 수 있음. 자바에는 없는 기능
+
+//    var int1 = 3
+//    var str4 = "int1 is $int1"
+//    val str5 = "int1 + 1 = ${int1+1}"
+//
+//    println("$str4  and $str5")
+
+    // 4. null
+//    var a : Int? = null
+//    var b : String? = "Hello"
+//
+//    println(a)
+//    println(b?.length)
+//    println(b!!.length) // 잘 사용하지 않는다.
+
+    // 5. 자료형의 비교와 연산
+//    val a : Int = 1
+//    val b : Double
+//    b = a.toDouble()
+//
+//    // 자료형 불일치 val c : Int =1.1
+//    val result = 1L+3 // 이 경우에는 Long 타입으로 인식 됨
+
+//    val a : Int = 128
+//    val b =128
+//    val c : Int? = a
+//    val d : Int? = c
+//
+//    val e : Int? = 128
+//    val f : Int? = b
+//    // 비교 연산
+//    println(a==b)
+//    println(a===b)
+//    println(a==c)
+//    println(a===c)
+//    println(c===d)
+//    println(e===f)
+//    println(a===f)
+
+//    var ex1 : Any = 3
+//    ex1 = 50L
+//    if(ex1 is Long){
+//        println("ex1 is Long")
+//    }
+
+    // 6. 연산자
+
+//    var a =10
+//    var b =10
+//
+//    var result = ++a
+//    var result2 = b++
+//    println("$result , $result2")
     
 }
