99469681ce87d14367dcb55371a9a464bab3b074
오진호 <71378447+ohjinhokor@users.noreply.github.com>
2022-01-03 08:50:00 +0900

    객체 지향 쿼리언어2(중급 문법) - 페치 조인

--- JPA/김영한님_강의/jpql/src/main/java/jpql/JpqlMain.java
+++ JPA/김영한님_강의/jpql/src/main/java/jpql/JpqlMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 import static jpql.MemberType.ADMIN;
@@ -318,6 +319,93 @@ public class JpqlMain {
 
             // select m from Member m join m.team t -> 명시적 조인
            // select m.team from Member m -> 묵시적 조인(내부 조인만 가능)
+
+
+
+            //페치조인 진짜 엄청중요하다!!!!
+            //객제지향 쿼리 언어2 - 중급 문법 - 페치조인
+
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setAge(10);
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(10);
+
+            member2.setType(ADMIN);
+            member2.setTeam(teamB);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+
+            String before_query = "select m From Member m";
+
+            //페치조인!!
+            String query = "select m From Member m join fetch m.team";
+
+//            List<Member> before_result = em.createQuery(before_query, Member.class).getResultList();
+
+//            for (Member member : before_result) {
+//                System.out.println("member = " + member.getUsername() + member.getTeam());
+//            }
+//            //회원1, 팀A(SQL)
+//            //회원2, 팀B(SQL)
+//            //회원3, 팀B(SQL)
+//
+//            //만약 회원 100명이 다 다른팀이라면 총 sql이 101번 나감. N+1문제 발생!!
+
+
+//            // 페치조인을 사용한 경우
+//            List<Member> fetch_result = em.createQuery(query, Member.class).getResultList();
+//
+//            for (Member member : fetch_result) {
+//                System.out.println("member = " + member.getUsername() +"," + member.getTeam().getName());
+//            }
+
+
+//            // 1:N 관계의 페치 조인
+//            // TeamA - 회원1
+//            // TeamB - 회원2
+//            // TeamB - 회원3
+//            // 이런식으로 TeamB의 row가 중복되어 나타난다.
+//            String query3 = "select t From Team t join fetch t.members";
+//
+//            List<Team> result = em.createQuery(query3, Team.class).getResultList();
+//            for (Team team : result) {
+//                System.out.println("team = " + team.getName() + ", member = " + team.getMembers().size());
+//                for(Member member : team.getMembers()){
+//                    System.out.println(" -> member = " + member.getUsername());
+//                }
+//            }
+
+            // 1:N 관계의 중복을 없애는 방법 1
+            String query4 = "select distinct t From Team t join fetch t.members";
+            List<Team> result = em.createQuery(query4, Team.class).getResultList();
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + ", member = " + team.getMembers().size());
+                for(Member member : team.getMembers()){
+                    System.out.println(" -> member = " + member.getUsername());
+                }
+            }
+
 
             tx.commit();
         } catch(Exception e){
