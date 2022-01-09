eaf95d8c6914b0ff5165d8015245e4452a62ccb4
오진호 <71378447+ohjinhokor@users.noreply.github.com>
2022-01-09 12:58:10 +0900

    공통 인터페이스 기능 - 순서 JPA기반 리포지토리 만들기

--- DataJpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ DataJpa/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -4,14 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-// @Rollback(false)로 하면 트랜잭션 롤백이 안됨
+//@Rollback(false)로 설정하면 트랙잭션 롤백 안함
 class MemberJpaRepositoryTest {
 
     @Autowired
@@ -29,4 +32,41 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(member);
 
     }
-}
+
+    @Test
+    public void basicCRUD(){
+
+        // 정석적인 테스트코드 스타일은 아니라고 함. 공부를 위한 간단한 테스트 코드임
+
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회
+        // 실제로 테스트코드를 작성할 때는 get을 쓰는게 썩 좋은 방법이 아닌듯?
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("new Memeber NAme!!!!!!");
+//        //리스트 조회
+//        List<Member> all = memberJpaRepository.findAll();
+//        assertThat(all.size()).isEqualTo(2);
+//
+//        //카운트 검증
+//        long count = memberJpaRepository.count();
+//        assertThat(count).isEqualTo(2);
+//
+//        //삭제 검증
+//        memberJpaRepository.delete(member1);
+//        memberJpaRepository.delete(member2);
+//
+//        List<Member> deltedCount = memberJpaRepository.findAll();
+//        assertThat(deltedCount.size()).isEqualTo(0);
+
+
+    }}
