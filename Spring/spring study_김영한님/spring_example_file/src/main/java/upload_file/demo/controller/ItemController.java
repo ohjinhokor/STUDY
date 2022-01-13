2f87b2ec12359a8e4204a330a5f6fe2e75be7a47
오진호 <71378447+ohjinhokor@users.noreply.github.com>
2022-01-13 19:55:11 +0900

    스프링-파일 업로드 및 다운로드 기능 예시

---
+++ Spring/spring study_김영한님/spring_example_file/src/main/java/upload_file/demo/controller/ItemController.java
@@ -0,0 +1,57 @@
+package upload_file.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import upload_file.demo.dtos.Item;
+import upload_file.demo.dtos.ItemForm;
+import upload_file.demo.dtos.UploadFile;
+import upload_file.demo.file.FileStore;
+import upload_file.demo.repository.ItemRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form){
+        return "item-form";
+    }
+
+    @PostMapping("/items/new")
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
+
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("items/{id}")
+    public String items(@PathVariable Long id, Model model){
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item", item);
+        return "item-view";
+    }
+}
---
+++ Spring/spring study_김영한님/spring_example_file/src/main/java/upload_file/demo/controller/MemberController.java
@@ -0,0 +1,47 @@
+package upload_file.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/photo")
+@Slf4j
+@RequiredArgsConstructor
+public class MemberController {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFile(@RequestParam String itemName,
+                           @RequestParam(value = "file") MultipartFile multipartFile , HttpServletRequest httpServletRequest) throws IOException {
+
+        log.info("request={}",httpServletRequest);
+        log.info("itemName={}",itemName);
+        log.info("multipartFile={}", multipartFile);
+
+        if(!multipartFile.isEmpty()){
+            String fullpath = fileDir + multipartFile.getOriginalFilename();
+            log.info("파일 저장 fullPath={}", fullpath);
+            System.out.println(fullpath);
+            multipartFile.transferTo(new File(fullpath));
+        }
+        return "upload-form";
+    }
+}
