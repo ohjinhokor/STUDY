7bb2eea2bbc57aff2bf2d9608060191e55ba645e
오진호 <71378447+ohjinhokor@users.noreply.github.com>
2022-01-20 01:37:46 +0900

    스프링 데이터 JPA 분석 - 새로운 엔티티를 구별하는 방법

--- DataJpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ DataJpa/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -44,7 +44,7 @@ public class MemberController {
         return map;
     }
 
-    @PostConstruct
+//    @PostConstruct
     public void init(){
         for(int i=0; i<100; i++){
             memberRepository.save(new Member("user"+i,i));
