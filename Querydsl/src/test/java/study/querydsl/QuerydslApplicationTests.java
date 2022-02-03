8bd38855e0c292b7b75059ba59276b8b810c8eb7
오진호 <71378447+ohjinhokor@users.noreply.github.com>
2022-02-03 23:59:14 +0900

    프로젝트 환경설정 - 스프링 부트 설정(모든 기본 설정 완료)

--- Querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ Querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Example;
 import study.querydsl.entity.QExample;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
