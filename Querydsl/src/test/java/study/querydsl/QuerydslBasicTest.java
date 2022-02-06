01b89d05c42290416f64f4d2f51d413dcfa35457
오진호 <71378447+ohjinhokor@users.noreply.github.com>
2022-02-06 20:55:20 +0900

    기본 문법 - 조인(기본조인)

--- Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -247,8 +247,6 @@ public class QuerydslBasicTest {
      * 팀의 이름과 각 팀의 평균 연령을 구해라
      *
      */
-
-
     @Test
     public void group() throws Exception{
         List<Tuple> result = queryFactory
@@ -266,5 +264,43 @@ public class QuerydslBasicTest {
 
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    //기본 문법 - 조인(기본 조인)
+
+    /**
+     * 팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join(){
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team) // 여기서 team은 사실 QTeam.team이다
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인
+     * 회원의 이름이 팀 이름과 같은 회원을 조회
+     */
+    @Test
+    public void theta_join(){
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
     }
 }
