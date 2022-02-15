392e0b402fcbe261cd4a5cfc1f55d79e123bee7e
ohjinhokor <ohjinho7@gmail.com>
2022-02-15 22:48:36 +0900

    sql function 호출하기

--- Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -827,4 +827,33 @@ public class QuerydslBasicTest {
                 .where(member.age.gt(10))
                 .execute();
     }
+
+    // 중급 문법 - sql function 호출하기
+    @Test
+    public void sqlFunction(){
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace', {0}, {1}, {2})",
+                        member.username, "member", "M"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("queryFactory = " + queryFactory);
+        }
+    }
+
+    @Test
+    public void sqlFunction2(){
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .where(member.username.eq(
+                        Expressions.stringTemplate("function('lower', {0})", member.username)))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
